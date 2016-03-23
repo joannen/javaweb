cbdebefa4effaa85ea1b7dbf788537cd061ead5e
Joanne Nori <joanne_nori@hotmail.com>
2016-03-23 12:29:47 +0100

    added teamprovider

--- web/src/main/java/se/threegorillas/provider/webuserparser/TeamProvider.java
+++ web/src/main/java/se/threegorillas/provider/webuserparser/TeamProvider.java
@@ -1,9 +1,20 @@
 package se.threegorillas.provider.webuserparser;
 
+import com.google.gson.*;
+import com.google.gson.stream.JsonWriter;
+import se.threegorillas.provider.WebTeam;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
+import java.io.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 
 /**
  * Created by joanne on 23/03/16.
@@ -12,7 +23,61 @@ import javax.ws.rs.ext.Provider;
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class TeamProvider {
+public class TeamProvider implements MessageBodyReader<WebTeam>, MessageBodyWriter<WebTeam> {
+
+    Gson gson = new GsonBuilder().registerTypeAdapter(WebTeam.class, new WebTeamAdapter()).create();
 
 
+    @Override
+    public boolean isReadable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return aClass.isAssignableFrom(WebTeam.class);
+    }
+
+    @Override
+    public WebTeam readFrom(Class<WebTeam> aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> multivaluedMap, InputStream inputStream) throws IOException, WebApplicationException {
+        return gson.fromJson(new InputStreamReader(inputStream), WebTeam.class);
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return  aClass.isAssignableFrom(WebTeam.class);
+    }
+
+    @Override
+    public long getSize(WebTeam webTeam, Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return 0;
+    }
+
+    @Override
+    public void writeTo(WebTeam webTeam, Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> multivaluedMap, OutputStream outputStream) throws IOException, WebApplicationException {
+
+        try(JsonWriter writer = new JsonWriter(new OutputStreamWriter(outputStream))){
+            gson.toJson(webTeam, WebTeam.class, writer);
+        }
+    }
+
+    private static final class WebTeamAdapter implements JsonSerializer<WebTeam>, JsonDeserializer<WebTeam>{
+
+        @Override
+        public WebTeam deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            JsonObject teamJson = jsonElement.getAsJsonObject();
+
+            Long id = teamJson.get("id").getAsLong();
+            String teamName = teamJson.get("teamName").getAsString();
+            String teamStatus = teamJson.get("teamStatus").getAsString();
+
+            return new WebTeam(id, teamName, teamStatus);
+        }
+
+        @Override
+        public JsonElement serialize(WebTeam webTeam, Type type, JsonSerializationContext jsonSerializationContext) {
+            JsonObject json = new JsonObject();
+
+            json.addProperty("id", webTeam.getId());
+            json.addProperty("teamName", webTeam.getTeamName());
+            json.addProperty("teamStatus", webTeam.getTeamStatus());
+
+            return json;
+        }
+    }
 }
