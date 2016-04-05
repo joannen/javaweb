f5e3cbe72ec886927f5fbb63e1f3a632a68f5719
frodrok <fredrik.hollinger@gmail.com>
2016-04-05 11:31:47 +0200

    added test to put

--- web/src/main/test/se/threegorillas/web/UserServiceTest.java
+++ web/src/main/test/se/threegorillas/web/UserServiceTest.java
@@ -1,20 +1,14 @@
 package se.threegorillas.web;
 
-import org.glassfish.jersey.server.ClientBinding;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import se.threegorillas.model.User;
 import se.threegorillas.provider.WebUser;
 import se.threegorillas.provider.webparser.UserProvider;
 
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import java.net.URI;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -26,7 +20,7 @@ public class UserServiceTest {
     private Client client;
     private WebTarget sampleUser;
     private WebTarget postUser;
-    private WebTarget getUser;
+    private WebTarget userWithId;
 
 
     @Before
@@ -34,7 +28,7 @@ public class UserServiceTest {
         client = ClientBuilder.newClient().register(UserProvider.class);
         sampleUser = client.target(url).path("sample");
         postUser = client.target(url);
-        getUser = client.target(url).path("{id}");
+        userWithId = client.target(url).path("{id}");
 
     }
 
@@ -65,6 +59,27 @@ public class UserServiceTest {
         WebUser retrievedUser = getUserWithId.request().get(WebUser.class);
         assertEquals(userToSave, retrievedUser);
 
+    }
+
+    @Test
+    public void updatedUserShouldBeUpdated() {
+        WebUser userToSave = new WebUser(1L, "abc", "abc", "abc", "abc", "abc");
+        URI location = postUser.request().post(Entity.entity(userToSave, MediaType.APPLICATION_JSON_TYPE)).getLocation();
+        WebTarget getUserWithId = client.target(location);
+        WebUser retrievedUser = getUserWithId.request().get(WebUser.class);
+        assertEquals(userToSave, retrievedUser);
+
+        WebUser updatedUser = new WebUser(retrievedUser.getId(), "cde", "cde", "cde", "cde", "cde");
+
+        int updateStatus = getUserWithId.request().put(Entity.entity(updatedUser, MediaType.APPLICATION_JSON_TYPE)).getStatus();
+        assertTrue(updateStatus == 204);
+
+        getUserWithId = client.target(location);
+
+        retrievedUser = getUserWithId.request().get(WebUser.class);
+        System.out.println(retrievedUser);
+
+        assertEquals(retrievedUser, updatedUser);
     }
 
 
