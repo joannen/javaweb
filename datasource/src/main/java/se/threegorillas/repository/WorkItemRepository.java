8c11a0074c366b056ff59472774e02a57a62eff7
frodrok <fredrik.hollinger@gmail.com>
2016-04-12 12:47:01 +0200

    added query to workitemrepository

--- datasource/src/main/java/se/threegorillas/repository/WorkItemRepository.java
+++ datasource/src/main/java/se/threegorillas/repository/WorkItemRepository.java
@@ -1,6 +1,7 @@
 package se.threegorillas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import se.threegorillas.model.WorkItem;
@@ -15,4 +16,7 @@ import java.util.Collection;
 public interface WorkItemRepository extends JpaRepository<WorkItem, Long> {
 
     Collection<WorkItem> findByStatus(String status);
+
+    @Query("select w from WorkItem w where w.description like ?1")
+    Collection<WorkItem> searchByDescription(String description);
 }
